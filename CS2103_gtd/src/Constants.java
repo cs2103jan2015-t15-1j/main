c2c8735073cd8e3a38e3fcba89b6dec672034962
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2015-02-18 22:30:38 +0800

    add class for constants and implement storage functions add, delete, update, done

---
+++ CS2103_gtd/src/Constants.java
@@ -0,0 +1,8 @@
+
+public final class Constants {
+    
+    public static final String MSG_ADDED = "Your task %1$s has been added";
+    public static final String MSG_DELETED = "The task %1$s has been deleted";
+    public static final String MSG_UPDATED = "The task %1$s has been updated";
+
+}
--- CS2103_gtd/src/Storage.java
+++ CS2103_gtd/src/Storage.java
@@ -6,27 +6,45 @@ import java.util.Stack;
 public class Storage {
     
     private static HashMap<Integer, Task> tasks = new HashMap<Integer, Task>();
-    private static Stack undoStack = new Stack(); 
+    private static Stack undoStack = new Stack();
     
     // Public methods
     public static String prepareStorage() {
         return "feedback";
     }
     
-    public static String add(String description, Date startDate, Date endDate) {
-        return "feedback";
+    public static String add(String desc, Date startDate, Date endDate) {
+        Task newTask = new Task(desc, startDate, endDate);
+        tasks.put(Task.getId(), Task);
+        return String.format(Constants.MSG_ADDED, Task.getUserFormat());
     }
     
     public static String delete(int id) {
-        return "feedback";
+        Task removedTask = tasks.get(id);
+        tasks.remove(id);
+        return String.format(Constants.MSG_DELETED, removedTask.getUserFormat());
     }
     
-    public static String update(int id) {
-        return "feedback";
+    public static String update(int id, String desc, Date startDate, Date endDate) {
+        Task updatedTask = tasks.get(id);
+        if (desc != null) {
+            updatedTask.setDescription(desc);
+        }
+        if (startDate != null) {
+            updatedTask.setStartDate(startDate);
+        }
+        if (endDate != null) {
+            updatedTask.setEndDate(endDate);
+        }
+        tasks.put(id, updatedTask);
+        return String.format(Constants.MSG_UPDATED, updatedTask.getUserFormat());
     }
     
     public static String done(int id) {
-        return "feedback";
+        Task doneTask = tasks.get(id);
+        doneTask.setDone(true);
+        tasks.put(id, doneTask);
+        return String.format(Constants.MSG_UPDATED, doneTask.getUserFormat());
     }
     
     public static String getTasks() {
