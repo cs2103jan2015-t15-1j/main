047a79d5e8fa8cafdaa5852b2a0c5898119e9a3d
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2015-02-18 21:51:38 +0800

    create mock object for storage

--- CS2103_gtd/src/Storage.java
+++ CS2103_gtd/src/Storage.java
@@ -1,4 +1,70 @@
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Stack;
+
 
 public class Storage {
+    
+    private static HashMap<Integer, Task> tasks = new HashMap<Integer, Task>();
+    private static Stack undoStack = new Stack(); 
+    
+    // Public methods
+    public static String prepareStorage() {
+        return "feedback";
+    }
+    
+    public static String add(String description, Date startDate, Date endDate) {
+        return "feedback";
+    }
+    
+    public static String delete(int id) {
+        return "feedback";
+    }
+    
+    public static String update(int id) {
+        return "feedback";
+    }
+    
+    public static String done(int id) {
+        return "feedback";
+    }
+    
+    public static String getTasks() {
+        return "feedback";
+    }
+    
+    public static String search(String keyword) {
+        return "task string or error message";
+    }
+    
+    public static String undo() {
+        return "feedback";
+    }
+    
+    public static String redo() {
+        return "feedback";
+    }
+
+    // Private methods
+    private static void initializeDataStructure() {
+        
+    }
+    
+    private static void getDataFromFile() {
+        
+    }
+    
+    private static void addUndo(COMMAND_TYPE ct, int taskId) {
+         
+    }
+    
+    private static void addRedo(COMMAND_TYPE ct, int taskId) {
+        
+    }
+    
+    private static void writeToFile() {
+        
+    }
+
 
 }
