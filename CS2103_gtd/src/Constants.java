73679c7ab78dbed12a16082071ad40e172257b9e
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2015-02-19 13:09:52 +0800

    implement getTasks(), search() and private methods for reading and writing to file

--- CS2103_gtd/src/Constants.java
+++ CS2103_gtd/src/Constants.java
@@ -4,5 +4,7 @@ public final class Constants {
     public static final String MSG_ADDED = "Your task %1$s has been added";
     public static final String MSG_DELETED = "The task %1$s has been deleted";
     public static final String MSG_UPDATED = "The task %1$s has been updated";
+    public static final String MSG_NO_TASKS = "You have currently no tasks.";
+    public static final String MSG_SEARCH_UNSUCCESSFUL = "No tasks containing your keyword was found";
 
 }
--- CS2103_gtd/src/Storage.java
+++ CS2103_gtd/src/Storage.java
@@ -1,12 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
+
+import org.json.*; //download: http://mvnrepository.com/artifact/org.json/json
 
 
 public class Storage {
     
-    private static HashMap<Integer, Task> tasks = new HashMap<Integer, Task>();
+    private static Map<Integer, Task> tasks = new HashMap<Integer, Task>();
     private static Stack undoStack = new Stack();
+    private static String filePath;
     
     // Public methods
     public static String prepareStorage() {
@@ -15,8 +25,8 @@ public class Storage {
     
     public static String add(String desc, Date startDate, Date endDate) {
         Task newTask = new Task(desc, startDate, endDate);
-        tasks.put(Task.getId(), Task);
-        return String.format(Constants.MSG_ADDED, Task.getUserFormat());
+        tasks.put(newTask.getId(), newTask);
+        return String.format(Constants.MSG_ADDED, newTask.getUserFormat());
     }
     
     public static String delete(int id) {
@@ -48,11 +58,28 @@ public class Storage {
     }
     
     public static String getTasks() {
-        return "feedback";
+        if (tasks.isEmpty()) {
+            return Constants.MSG_NO_TASKS;
+        }
+        String allTasks = "";
+        for (Task task : tasks.values()) {
+            allTasks += task.getId() + ". " + task.getUserFormat();
+        }
+        return allTasks;
     }
     
     public static String search(String keyword) {
-        return "task string or error message";
+        String searchResult = "";
+        for (Task task : tasks.values()) {
+            String taskDesc = task.getUserFormat();
+            if (taskDesc.toLowerCase().contains(keyword.toLowerCase())) {
+                searchResult += task.getId() + ". " + task.getUserFormat();
+            }
+        }
+        if (searchResult == "") {
+            return Constants.MSG_SEARCH_UNSUCCESSFUL;
+        }
+        return searchResult;
     }
     
     public static String undo() {
@@ -64,12 +91,90 @@ public class Storage {
     }
 
     // Private methods
-    private static void initializeDataStructure() {
-        
+    private static void setFilePath(String fileName) {
+        //todo: check if file/filename is valid
+        filePath = System.getProperty("user.dir") + "/" + fileName;
+        File file = new File(filePath);
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     private static void getDataFromFile() {
+        jsonStr = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            String line = br.readLine();
+            while (line != null) {
+                jsonStr += " " + line;
+                line = br.readLine();
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        createTasksFromJson(jsonStr);
+    }
+    
+    /* JSON example:
+     * { tasks: [
+     *      {
+     *          desc: do something,
+     *          startDate: 190215
+     *          endDate: 200215
+     *          done: false
+     *      },
+     *      { ... },
+     *      { ... },
+     *      ...]
+     *  }
+     */
+    private static void createTasksFromJson(String jsonStr) {
+        JSONObject jsonObj = new JSONObject(jsonStr);
+        JSONArray jsonArr = jsonObj.getJSONArray("tasks");
+        for (int i = 0; i < jsonArr.length(); i++)  {
+            currentObj = arr.getInt(i);
+            String desc = jsonObj.getString("desc");
+            Date startDate = jsonObj.getDate("startDate");
+            Date startDate = jsonObj.getDate("endDate");
+            boolean done = jsonObj.getBoolean("done");
+            Task newTask = new Task(desc, startDate, endDate, done); 
+            tasks.put(i, newTask);
+        }
+    }
+    
+    private static void writeToFile() {
+        JSONObject jsonObj = new JSONObject();
+ 
+        JSONArray jsonArray = new JSONArray();
+        for (Task task : tasks.values()) {
+            JSONObject taskObj = new JSONObject();
+            String desc = task.getDescription();
+            taskObj.put("desc: "+desc);
+            Date startDate = task.getStartDate().toString();
+            taskObj.put("startDate: "+startDate);
+            Date endDate = task.getStartDate().toString();
+            taskObj.put("endDate: "+endDate);
+            String done = task.getDone().toString();
+            taskObj.put("done: "+done);
+            jsonArray.add(taskObj);
+        }
         
+        jsonObj.put("tasks", jsonArray);
+ 
+        FileWriter file = new FileWriter(filePath);
+        try {
+            file.write(jsonObj.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            file.flush();
+            file.close();
+        }
     }
     
     private static void addUndo(COMMAND_TYPE ct, int taskId) {
@@ -77,10 +182,6 @@ public class Storage {
     }
     
     private static void addRedo(COMMAND_TYPE ct, int taskId) {
-        
-    }
-    
-    private static void writeToFile() {
         
     }
 
