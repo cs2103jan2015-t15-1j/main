4b3b0cc5874aaec858b01e4c871aea92df03afee
Luqman <lnhakim10@hotmail.com>
2015-03-04 10:51:09 +0800

    created non-functional Task

--- CS2103_gtd/src/Task.java
+++ CS2103_gtd/src/Task.java
@@ -1,4 +1,65 @@
 
 public class Task {
 
+	private int idNumber;
+	private String description;
+	private Date startDate, endDate;
+	private boolean done;
+	
+	public void Task(String descr, Date start, Date end){
+		description = descr;
+		startDate = start;
+		endDate = end;
+		
+	}
+	
+	public int getIdNumber(){
+		
+	}
+	
+	public String getDescription(){ 
+		
+	}
+	
+	public Date getStartDate(){
+		
+	}
+	
+	public Date getEndDate(){
+		
+	}
+	
+	public boolean getDone(){
+		
+	}
+	
+	public String getStorageFormat(){
+		
+	}
+	
+	public String getUserFormat(){
+		
+	}
+	
+	public boolean setIdNumber(int newIdNumber){
+		
+	}
+	
+	public boolean setDescription(String newDescription){
+		
+	}
+	
+	public boolean setStartDate(Date newStartDate){
+		
+	}
+	
+	public boolean setEndDate(Date newEndDate){
+		
+	}
+	
+	public boolean setDone(boolean isDone){
+		
+	}
+	
+
 }
