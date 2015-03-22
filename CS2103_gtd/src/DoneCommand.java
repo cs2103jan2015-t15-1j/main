86eac7fb25502abf5e638fa4261c7055b583929e
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2015-03-22 16:13:40 +0800

    implement DoneCommand

---
+++ CS2103_gtd/src/DoneCommand.java
@@ -0,0 +1,23 @@
+
+public class DoneCommand implements Command {
+    
+    int taskId;
+    boolean setDone;
+    
+    public DoneCommand(int _id, boolean _setDone){
+        taskId = _id;
+        setDone = _setDone;
+    }
+    
+    @Override
+    public String execute(Storage storage) {
+        String userFeedback = storage.done(taskId);
+        return userFeedback;
+    }
+
+    @Override
+    public Command makeUndo(Storage storage) {
+        return new DoneCommand(taskId, !setDone);
+    }
+
+}
