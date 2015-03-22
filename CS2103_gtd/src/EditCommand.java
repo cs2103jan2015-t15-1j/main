e8831c78c5776f916ac93fc1428b693abb377523
Luqman <lnhakim10@hotmail.com>
2015-03-22 14:34:43 +0800

    Implemented editcommand.makeundo

--- CS2103_gtd/src/EditCommand.java
+++ CS2103_gtd/src/EditCommand.java
@@ -3,7 +3,6 @@ import java.util.logging.Logger;
 
 class EditCommand implements Command {
 	Task _newTask;
-	Task _oldTask = new Task();
 
 	private static final Logger logger = Logger.getLogger(EditCommand.class
 			.getName());
@@ -17,28 +16,28 @@ class EditCommand implements Command {
 	@Override
 	public String execute(Storage storage) {
 		int TaskId = _newTask.getId();
-		makeShallowCopyOfOriginalTask(storage, TaskId);
 		String feedback = storage.update(TaskId, _newTask);
 		logger.log(Level.FINE, storage.getTask(TaskId).getUserFormat());
 		return feedback;
 	}
 
 	@Override
-	public String undo(Storage storage) {
+	public Command makeUndo(Storage storage) {
 		int TaskId = _newTask.getId();
-		String feedback = storage.update(TaskId, _oldTask);
-		logger.log(Level.FINE, storage.getTask(TaskId).getUserFormat());
-		return feedback;
+		Task oldTask = makeShallowCopyOfOriginalTask(storage, TaskId);
+		return new EditCommand(oldTask);
 	}
 
-	private void makeShallowCopyOfOriginalTask(Storage storage, int TaskId) {
+	private Task makeShallowCopyOfOriginalTask(Storage storage, int TaskId) {
+		Task oldTask = new Task();
 		Task old = storage.getTask(TaskId);
-		_oldTask.setDescription(old.getDescription());
-		_oldTask.setDone(old.getDone());
-		_oldTask.setId(old.getId());
-		_oldTask.setStartDateTime(old.getStartDateTime());
-		_oldTask.setEndDateTime(old.getEndDateTime());
-		_oldTask.setLocation(old.getLocation());
+		oldTask.setDescription(old.getDescription());
+		oldTask.setDone(old.getDone());
+		oldTask.setId(old.getId());
+		oldTask.setStartDateTime(old.getStartDateTime());
+		oldTask.setEndDateTime(old.getEndDateTime());
+		oldTask.setLocation(old.getLocation());
+		return oldTask;
 	}
 
 }
