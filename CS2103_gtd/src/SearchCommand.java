ecf8c2e4d51834fbcfc35964228a7906d0758935
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2015-03-22 16:32:17 +0800

    implement SearchCommand

---
+++ CS2103_gtd/src/SearchCommand.java
@@ -0,0 +1,21 @@
+
+public class SearchCommand implements Command {
+    
+    Task searchObj;
+    
+    public SearchCommand(Task _searchObj) {
+        searchObj = _searchObj;
+    }
+    
+    @Override
+    public String execute(Storage storage) {
+        String searchFeedback = storage.search(searchObj);
+        return searchFeedback;
+    }
+
+    @Override
+    public Command makeUndo(Storage storage) {
+        return null;
+    }
+
+}
