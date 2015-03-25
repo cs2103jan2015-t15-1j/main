3b8540c408131cc2717e411912e9139db6c2961f
Hanbin Cho <a0135295@u.nus.edu>
2015-03-25 18:26:22 +0800

    Translator interprets user input "clear" to make ClearCommand(). Cleaned up Translator code a bit to make it more readable.

--- CS2103_gtd/src/Translator.java
+++ CS2103_gtd/src/Translator.java
@@ -5,9 +5,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Translator {
-	
-	private static final String INVALID_COMMAND = "%1$s is not a valid command!";
-	// Keywords
+	//==========Constants for Translator class Section Beginning==========//
+	// Keywords for ADD command
 	private static final String KEYWORD_ADD_DEADLINE = "((by)|(BY)|(due)|(DUE))";
 	private static final String KEYWORD_ADD_LOCATION = "((at)|(AT)|(@))";
 	private static final String KEYWORD_ADD_EVENTSTART = "((from)|(FROM)|(start)|(START))";
@@ -15,6 +14,7 @@ public class Translator {
 	private static final String[] addParameterKeywords = 
 		{KEYWORD_ADD_DEADLINE, KEYWORD_ADD_LOCATION, KEYWORD_ADD_EVENTSTART, KEYWORD_ADD_EVENTEND};
 	
+	// Keywords for SEARCH command
 	private static final String KEYWORD_SEARCH_DUE = "((due)|(DUE))";
 	private static final String KEYWORD_SEARCH_AFTER = "((after)|(AFTER))";
 	private static final String KEYWORD_SEARCH_BEFORE = "((before)|(BEFORE))";
@@ -22,6 +22,7 @@ public class Translator {
 	private static final String[] searchParameterKeywords = 
 		{KEYWORD_SEARCH_DUE, KEYWORD_SEARCH_AFTER, KEYWORD_SEARCH_BEFORE, KEYWORD_SEARCH_ON};
 	
+	// Keywords for EDIT command
 	private static final String KEYWORD_EDIT_DEADLINE = "((deadline)|(DEADLINE))";
 	private static final String KEYWORD_EDIT_LOCATION = "((location)|(LOCATION))";
 	private static final String KEYWORD_EDIT_EVENTSTART = "((start)|(START))";
@@ -31,26 +32,36 @@ public class Translator {
 		{KEYWORD_EDIT_DEADLINE, KEYWORD_EDIT_LOCATION, KEYWORD_EDIT_EVENTSTART, KEYWORD_EDIT_EVENTEND,
 		KEYWORD_EDIT_DESCRIPTION};
 	
+	
+	// Format for Date-Time input.
 	private static final String DELIMITTER_DATE = "(\\s|-|/)";
 	private static final String DD_MM_YYYY = "\\d\\d" + DELIMITTER_DATE + "\\d\\d" +
 			DELIMITTER_DATE + "\\d\\d\\d\\d";
 	private static final String DELIMITTER_TIME = "(:)";
 	private static final String HH_MM = "(0|1|2)\\d" + DELIMITTER_TIME + "([0-5])\\d";
 	
-	private static final String TIME_SEARCH_DEFAULT = " 12:00 ";
+	
+	// Default values for Date-Time variables.
+	private static final String FILLER_DEFAULT_TIME = " 12:00 ";
 	private static final int DATETIME_HOUR_MINIMUM = 0;
 	private static final int DATETIME_MINUTE_MINIMUM = 0;
 	private static final int DATETIME_HOUR_MAXIMUM = 23;
 	private static final int DATETIME_MINUTE_MAXIMUM = 59;
 	
+	// Miscellaneous default values.
 	private static final String EMPTY_STRING = "";
 	private static final String SINGLE_SPACE = " ";
 	private static final String WHITESPACE = "\\s+";
 	private static final int ARRAY_POSITION_FIRST = 0;
 	private static final int ARRAY_POSITION_SECOND = 1;
+	private static final int NUM_TASKID_MINIMUM = 1;
+	
+	// Error values.
+	private static final String INVALID_COMMAND = "%1$s is not a valid command!";
 	private static final String PARAMETER_DOES_NOT_EXIST = null;
 	private static final int INT_PARAM_INVALID = -1;
 	private static final int INVALID_TASK_ID = -1;
+	//==========Constants for Translator class Section End================//
 	
 	public Translator() {
 		
@@ -60,6 +71,7 @@ public class Translator {
 		Command newCommand = null;
 		String inputCommandTypeString = extractFirstWord(usercommand);
 		CommandType inputCommandType = determineCommandType(inputCommandTypeString);
+		
 		switch (inputCommandType) {
 		case ADD :
 			newCommand = createAddCommand(usercommand);
@@ -94,15 +106,16 @@ public class Translator {
 			newCommand = createSearchCommand(usercommand);
 			break;
 		case CLEAR :
+			newCommand = createClearCommand();
 			break;
 		default :
 			throw new Exception (String.format(INVALID_COMMAND, inputCommandTypeString));
 		}
+		
 		return newCommand;
 	}
 	
 	private CommandType determineCommandType(String commandTypeString) {
-		//ADD, DISPLAY, DONE, DELETE, EDIT, UNDO, REDO, HELP, SETDIR, EXIT
 		if (commandTypeString.equalsIgnoreCase("ADD")) {
 			return CommandType.ADD;
 		} else if (commandTypeString.equalsIgnoreCase("DISPLAY")) {
@@ -137,6 +150,15 @@ public class Translator {
 		Task[] addInformation = {interpretAddParameter(usercommand)};
 		return new AddCommand(addInformation);
 	}
+
+	private Command createDisplayCommand() {
+		return new DisplayCommand();
+	}
+	
+	private Command createSearchCommand(String usercommand) {
+		Task searchInformation = interpretSearchParameter(usercommand);
+		return new SearchCommand(searchInformation);
+	}
 	
 	private Command createEditCommand(String usercommand) {
 		Task editInformation = interpretEditParameter(usercommand);
@@ -148,22 +170,13 @@ public class Translator {
 		return new DeleteCommand(deleteInformation);
 	}
 	
+	private Command createClearCommand() {
+		return new ClearCommand();
+	}
+	
 	private Command createDoneCommand(String usercommand) {
 		int[] doneInformation = interpretDoneParameter(usercommand);
 		return new DoneCommand(doneInformation, true);
-	}
-	
-	private Command createSearchCommand(String usercommand) {
-		Task searchInformation = interpretSearchParameter(usercommand);
-		return new SearchCommand(searchInformation);
-	}
-	
-	private Command createDisplayCommand() {
-		return new DisplayCommand();
-	}
-	
-	private Command createHelpCommand() {
-		return new HelpCommand();
 	}
 	
 	private Command createUndoCommand() {
@@ -173,6 +186,17 @@ public class Translator {
 	private Command createRedoCommand() {
 		return new RedoCommand();
 	}
+
+	private Command createHelpCommand() {
+		return new HelpCommand();
+	}
+	
+	/*
+	private Command createSetDirectoryCommand(String usercommand) {
+		// interpretFilePath
+		return new SetDirectoryCommand()
+	}
+	*/
 	
 	private Command createExitCommand() {
 		return new ExitCommand();
@@ -180,90 +204,93 @@ public class Translator {
 	
 	private Task interpretAddParameter(String usercommand) {
 		
-		KeywordInfoList kList = null;
 		Task newTask = new Task();
-		
-			kList = new KeywordInfoList(usercommand, addParameterKeywords);
-		
-			String paramDescription = kList.getDescription();
-			String paramDeadline = kList.getParameter(KEYWORD_ADD_DEADLINE);
-			String paramLocation = kList.getParameter(KEYWORD_ADD_LOCATION);
-			String paramEventStart = kList.getParameter(KEYWORD_ADD_EVENTSTART);
-			String paramEventEnd = kList.getParameter(KEYWORD_ADD_EVENTEND);
-			
-			if (paramDescription != EMPTY_STRING) {
-				newTask.setDescription(paramDescription);
-				
-				if (paramLocation != PARAMETER_DOES_NOT_EXIST) {
-					newTask.setLocation(paramLocation);
-				}
-				
-				if (paramDeadline != PARAMETER_DOES_NOT_EXIST) {
-					// task type is a deadline
-					LocalDateTime deadline = interpretDateTimeParam(paramDeadline);
-					newTask.setEndDateTime(deadline);
-					
-				} else if (paramEventStart != PARAMETER_DOES_NOT_EXIST && paramEventEnd != PARAMETER_DOES_NOT_EXIST) {
-					// task type is an event
-					LocalDateTime eventStart = interpretDateTimeParam(paramEventStart);
-					LocalDateTime eventEnd = interpretDateTimeParam(paramEventEnd);
-					newTask.setStartDateTime(eventStart);
-					newTask.setEndDateTime(eventEnd);
-					
-				} else {
-					// task type is floating
-					// no more action needed.
-				}
+		KeywordInfoList kList = new KeywordInfoList(usercommand, addParameterKeywords);
+
+		String paramDescription = kList.getDescription();
+		String paramLocation = kList.getParameter(KEYWORD_ADD_LOCATION);
+		String paramDeadline = kList.getParameter(KEYWORD_ADD_DEADLINE);
+		String paramEventStart = kList.getParameter(KEYWORD_ADD_EVENTSTART);
+		String paramEventEnd = kList.getParameter(KEYWORD_ADD_EVENTEND);
+
+		if (paramDescription != EMPTY_STRING) {
+			newTask.setDescription(paramDescription);
+
+			if (paramLocation != PARAMETER_DOES_NOT_EXIST) {
+				newTask.setLocation(paramLocation);
 			}
-		
+
+			if (paramDeadline != PARAMETER_DOES_NOT_EXIST) {
+				// The task type is "deadline"
+				LocalDateTime deadline = interpretDateTimeParam(paramDeadline);
+				newTask.setEndDateTime(deadline);
+
+			} else if (paramEventStart != PARAMETER_DOES_NOT_EXIST && paramEventEnd != PARAMETER_DOES_NOT_EXIST) {
+				// The task type is "event"
+				LocalDateTime eventStart = interpretDateTimeParam(paramEventStart);
+				LocalDateTime eventEnd = interpretDateTimeParam(paramEventEnd);
+				newTask.setStartDateTime(eventStart);
+				newTask.setEndDateTime(eventEnd);
+
+			} else {
+				// The task type is "floating." No further action necessary.
+			}
+		}
+
 		return newTask;
 	}
 	
 	private Task interpretEditParameter(String usercommand) {
-		KeywordInfoList kList = null;
-		Task newTask = new Task();
-		kList = new KeywordInfoList(usercommand, editParameterKeywords);
 		
+		Task newTask = new Task();
+		KeywordInfoList kList = new KeywordInfoList(usercommand, editParameterKeywords);
+
 		int taskID = extractEditTaskID(usercommand);
 		if (taskID != INVALID_TASK_ID) {
-			
+
 			newTask.setId(taskID);
-			boolean doesParameterExists = false;
-			
+			boolean doesEditParameterExist = false;
+
 			String paramDescription = kList.getParameter(KEYWORD_EDIT_DESCRIPTION);
-			String paramDeadline = kList.getParameter(KEYWORD_EDIT_DEADLINE);
 			String paramLocation = kList.getParameter(KEYWORD_EDIT_LOCATION);
+			String paramDeadline = kList.getParameter(KEYWORD_EDIT_DEADLINE);
 			String paramEventStart = kList.getParameter(KEYWORD_EDIT_EVENTSTART);
 			String paramEventEnd = kList.getParameter(KEYWORD_EDIT_EVENTEND);
-			
-			if (paramDescription != null) {
+
+			if (paramDescription != PARAMETER_DOES_NOT_EXIST) {
 				newTask.setDescription(paramDescription);
-				doesParameterExists = true;
+				doesEditParameterExist = true;
 			}
-			if (paramDeadline != null) {
+			
+			if (paramLocation != PARAMETER_DOES_NOT_EXIST) {
+				newTask.setLocation(paramLocation);
+				doesEditParameterExist = true;
+			}
+			
+			if (paramDeadline != PARAMETER_DOES_NOT_EXIST) {
 				LocalDateTime deadline = interpretDateTimeParam(paramDeadline);
 				newTask.setEndDateTime(deadline);
-				doesParameterExists = true;
+				doesEditParameterExist = true;
+				
 			} else {
-				if (paramEventStart != null) {
+				if (paramEventStart != PARAMETER_DOES_NOT_EXIST) {
 					LocalDateTime eventStart = interpretDateTimeParam(paramEventStart);
 					newTask.setStartDateTime(eventStart);
-					doesParameterExists = true;
+					doesEditParameterExist = true;
 				}
-				if (paramEventEnd != null) {
+				
+				if (paramEventEnd != PARAMETER_DOES_NOT_EXIST) {
 					LocalDateTime eventEnd = interpretDateTimeParam(paramEventEnd);
 					newTask.setEndDateTime(eventEnd);
-					doesParameterExists = true;
+					doesEditParameterExist = true;
 				}
 			}
-			if (paramLocation != null) {
-				newTask.setLocation(paramLocation);
-				doesParameterExists = true;
-			}
-			if (doesParameterExists == true) {
+			
+			if (doesEditParameterExist == true) {
 				return newTask;
 			}
 		}
+		
 		return null;
 	}
 	
@@ -272,6 +299,9 @@ public class Translator {
 		int taskID;
 		try {
 			taskID = Integer.parseInt(taskIDString);
+			if (taskID < NUM_TASKID_MINIMUM) {
+				taskID = INVALID_TASK_ID;
+			}
 		} catch (NumberFormatException e) {
 			taskID = INVALID_TASK_ID;
 		}
@@ -282,37 +312,38 @@ public class Translator {
 		
 		Task newTask = new Task();
 		KeywordInfoList kList = new KeywordInfoList(usercommand, searchParameterKeywords);
-		//String paramDue = kList.getParameter(KEYWORD_SEARCH_DUE);
+		
+		String paramDescription = kList.getDescription();
 		String paramAfter = kList.getParameter(KEYWORD_SEARCH_AFTER);
 		String paramBefore = kList.getParameter(KEYWORD_SEARCH_BEFORE);
-		//String paramOn = kList.getParameter(KEYWORD_DISPLAY_ON);
-		String paramDescription = kList.getDescription();
 		String paramOn = kList.getParameter(KEYWORD_SEARCH_ON);
 		
-		// Storage can take advantage of LocalDateTime.isAfter, LocalDateTime.isBefore.
-		// Convention must be set up on how to tell Storage that this Display wants before/after/due/on.
-		
-		if (paramDescription == null) {
+		if (paramDescription == PARAMETER_DOES_NOT_EXIST) {
 			newTask.setDescription(EMPTY_STRING);
+			
 		} else {
 			newTask.setDescription(paramDescription);
 		}
-		if (paramOn == null) {
-			if (paramAfter == null) {
+		
+		if (paramOn == PARAMETER_DOES_NOT_EXIST) {
+			if (paramAfter == PARAMETER_DOES_NOT_EXIST) {
 				newTask.setStartDateTime(LocalDateTime.MIN);
+				
 			} else {
 				LocalDateTime lowerBoundaryTime = interpretDateTimeParam(paramAfter);
 				newTask.setStartDateTime(lowerBoundaryTime);
 			}
-			if (paramBefore == null) {
+			
+			if (paramBefore == PARAMETER_DOES_NOT_EXIST) {
 				newTask.setEndDateTime(LocalDateTime.MAX);
+				
 			} else {
 				LocalDateTime upperBoundaryTime = interpretDateTimeParam(paramBefore);
 				newTask.setEndDateTime(upperBoundaryTime);
 			}
+			
 		} else {
-			// Assuming the user only inputs DD/MM/YYYY but no HH:mm
-			paramOn += TIME_SEARCH_DEFAULT;
+			paramOn += FILLER_DEFAULT_TIME;
 			LocalDateTime dayToSearch = interpretDateTimeParam(paramOn);
 			newTask.setStartDateTime(getBeginningOfDay(dayToSearch));
 			newTask.setEndDateTime(getEndOfDay(dayToSearch));
@@ -323,15 +354,15 @@ public class Translator {
 
 	
 	private LocalDateTime getBeginningOfDay(LocalDateTime dateTime) {
-		LocalDateTime alteredHour = dateTime.withHour(DATETIME_HOUR_MINIMUM);
-		LocalDateTime alteredHourAndMinute = alteredHour.withMinute(DATETIME_MINUTE_MINIMUM);
-		return alteredHourAndMinute;
+		LocalDateTime minuteSetToMinimum = dateTime.withMinute(DATETIME_MINUTE_MINIMUM);
+		LocalDateTime hourAndMinuteSetToMinimum = minuteSetToMinimum.withHour(DATETIME_HOUR_MINIMUM);
+		return hourAndMinuteSetToMinimum;
 	}
 	
 	private LocalDateTime getEndOfDay(LocalDateTime dateTime) {
-		LocalDateTime alteredHour = dateTime.withHour(DATETIME_HOUR_MAXIMUM);
-		LocalDateTime alteredHourAndMinute = alteredHour.withMinute(DATETIME_MINUTE_MAXIMUM);
-		return alteredHourAndMinute;
+		LocalDateTime minuteSetToMaximum = dateTime.withMinute(DATETIME_MINUTE_MAXIMUM);
+		LocalDateTime hourAndMinuteSetToMaximum = minuteSetToMaximum.withHour(DATETIME_HOUR_MAXIMUM);
+		return hourAndMinuteSetToMaximum;
 	}
 	
 	private int[] interpretDeleteParameter(String usercommand) {
@@ -345,40 +376,49 @@ public class Translator {
 	}
 	
 	private LocalDateTime interpretDateTimeParam(String param) {
-		// NEED TO BE IMPLEMENTED:
-		// Difference between Add and Edit: Add will fill in unspecified fields of LocalDateTime by default, whereas
-		// Edit will only fill in fields of LocalDateTime that need to be changed.
-		
-		// Date example: 02-03-2015
+		// Date format: dd-MM-yyyy
 		LocalDate date = extractLocalDate(param);
-		// Time example: 8PM | 8:00 | 20:00 | 8
+		// Time format: HH:mm
 		LocalTime time = extractLocalTime(param);
-		
 		return LocalDateTime.of(date, time);
 	}
 	
 	private LocalDate extractLocalDate(String param) {		
-		Pattern p = Pattern.compile(DD_MM_YYYY);
-		Matcher m = p.matcher(param);
-		if (m.find()) {
-			String dateString = m.group();
+		Pattern datePattern = Pattern.compile(DD_MM_YYYY);
+		Matcher datePatternMatcher = datePattern.matcher(param);
+		if (datePatternMatcher.find()) {
+			String dateString = datePatternMatcher.group();
 			String[] dateSegments = dateString.split(DELIMITTER_DATE);
-			// Following Singaporean date convention: day, month, year
-			return LocalDate.of(Integer.parseInt(dateSegments[2]), 
-					Integer.parseInt(dateSegments[1]), Integer.parseInt(dateSegments[0]));
+			try {
+				int day = Integer.parseInt(dateSegments[0]);
+				int month = Integer.parseInt(dateSegments[1]);
+				int year = Integer.parseInt(dateSegments[2]);
+				return LocalDate.of(year, month, day);
+			} catch (NumberFormatException e) {
+				// Program should never reach here.
+				// Suggest using assert to check that dateSegments are all parse-able?
+				return null;
+			}
 		} else {
 			return null;
 		}
 	}
 	
 	private LocalTime extractLocalTime(String param) {		
-		Pattern p = Pattern.compile(HH_MM);
-		Matcher m = p.matcher(param);
-		if (m.find()) {
-			String timeString = m.group();
+		Pattern timePattern = Pattern.compile(HH_MM);
+		Matcher timePatternMatcher = timePattern.matcher(param);
+		if (timePatternMatcher.find()) {
+			String timeString = timePatternMatcher.group();
 			String[] timeSegments = timeString.split(DELIMITTER_TIME);
-			// Following Singaporean time convention: hour:minute
-			return LocalTime.of(Integer.parseInt(timeSegments[0]), Integer.parseInt(timeSegments[1]));
+			try {
+				int hour = Integer.parseInt(timeSegments[0]);
+				int minute = Integer.parseInt(timeSegments[1]);
+				return LocalTime.of(hour, minute);
+			} catch (NumberFormatException e) {
+				// Program should never reach here.
+				// Suggest using assert to check that timeSegments are all parse-able?
+				return null;
+			}
 		} else {
 			return null;
 		}
@@ -388,16 +428,18 @@ public class Translator {
 		str = str.trim();
 		if (str.equals(EMPTY_STRING)) {
 			return EMPTY_STRING;
+		} else {
+			return str.split(WHITESPACE)[ARRAY_POSITION_FIRST];
 		}
-		return str.split(WHITESPACE)[ARRAY_POSITION_FIRST];
 	}
 	
 	private String extractSecondWord(String str) {
 		str = str.trim();
 		if (str.equals(EMPTY_STRING)) {
 			return EMPTY_STRING;
+		} else {
+			return str.split(WHITESPACE)[ARRAY_POSITION_SECOND];
 		}
-		return str.split(WHITESPACE)[ARRAY_POSITION_SECOND];
 	}
 	
 	private String removeFirstWord(String str) {
@@ -405,11 +447,10 @@ public class Translator {
 		int firstSpacePos = str.indexOf(SINGLE_SPACE);
 		if (0 < firstSpacePos) {
 			str = str.substring(firstSpacePos);
-			str = str.trim();
+			return str.trim();
 		} else {
-			str = EMPTY_STRING;
+			return EMPTY_STRING;
 		}
-		return str;
 	 }
 	
 	// return int array of -1's if the input is not valid (all args must be integer)
