745831bd1eab929a95a17f1b6ee8d65cc805a340
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2015-04-01 23:40:14 +0800

    extracted search functionality in Storage to its own class StorageSearch

--- CS2103_gtd/src/Storage.java
+++ CS2103_gtd/src/Storage.java
@@ -6,9 +6,11 @@ public class Storage {
     
     private Map<Integer, Task> tasks = new HashMap<Integer, Task>();
     private StorageIO storageIO;
+    private StorageSearch storageSearch;
     private int lastIdNumber;
     
     public String prepareStorage(String fileName) {
+        storageSearch = new StorageSearch();
         storageIO = new StorageIO();
         String filePath = storageIO.setFilePath(fileName);
         storageIO.getDataFromFile(tasks);
@@ -161,102 +163,12 @@ public class Storage {
     }
     
     public String search(Task searchObj) {
-        String keyword = searchObj.getDescription();
-        LocalDateTime startDate = searchObj.getStartDateTime();
-        LocalDateTime endDate = searchObj.getEndDateTime();
-        
-        int[] foundTasks = new int[lastIdNumber];
-        foundTasks = searchOnKeyword(keyword, foundTasks);
-        foundTasks = searchOnDate(startDate, endDate, foundTasks);
-
-        String searchResult = "";
-        for (int i=0; i<foundTasks.length; i++) {
-            if (foundTasks[i] == Constants.INCLUDED_IN_SEARCH) {
-                Task task = tasks.get(i+1);
-                searchResult += "\n" + task.getUserFormat();
-            }
-        }
-        
-        if (searchResult.equals("")) {
-            return Constants.MESSAGE_SEARCH_UNSUCCESSFUL;
-        }
-        return Constants.DISPLAY_TABLE_HEADERS+"\n"+searchResult;
-    }
-    
-    private int[] searchOnKeyword(String keyword, int[] foundTasks) {
-        for (Task task : tasks.values()) {
-            if (!task.getDone()) { 
-                String taskDesc = task.getDescription();
-                if (taskDesc.toLowerCase().contains(keyword.toLowerCase())) {
-                    int index = task.getId()-1;
-                    foundTasks[index] = Constants.INCLUDED_IN_SEARCH;
-                }
-            }
-        }
-        return foundTasks;
-    }
-    
-    private int[] searchOnDate(LocalDateTime startDate, LocalDateTime endDate, 
-            int[] foundTasks) {
-        if (isDateSearch(startDate, endDate)) {
-            for (Task task : tasks.values()) {
-                if (!task.getDone()) {
-                    int index = task.getId()-1;
-                    foundTasks[index] = isTaskInInterval(task, startDate, 
-                            endDate, foundTasks[index]);
-                }
-            }
-        }
-        return foundTasks;
-    }
-    
-    private int isTaskInInterval(Task task, LocalDateTime searchStartDate, 
-            LocalDateTime searchEndDate, int originalValue) {
-        LocalDateTime taskStartDate = task.getStartDateTime();
-        LocalDateTime taskEndDate = task.getEndDateTime();
-        boolean startIsAfter = false;
-        boolean startIsOn = false;
-        boolean endIsBefore = false;
-        boolean endIsOn = false;
-        
-        if (isFloatingTask(taskEndDate)) {
-            return Constants.NOT_INCLUDED_IN_SEARCH;
-        } else if (isDeadlineTask(taskStartDate)) {
-            startIsAfter = taskEndDate.isAfter(searchStartDate);
-            startIsOn = taskEndDate.equals(searchStartDate);
-        } else {
-            startIsAfter = taskStartDate.isAfter(searchStartDate);
-            startIsOn = taskStartDate.equals(searchStartDate);
-        }
-        endIsBefore = taskEndDate.isBefore(searchEndDate);
-        endIsOn = taskEndDate.equals(searchEndDate);
-        
-        if (isNotInInterval(startIsAfter, startIsOn, endIsBefore, endIsOn)) {
-            return Constants.NOT_INCLUDED_IN_SEARCH;
-        }
-        return originalValue;
-    }
-    
-    private boolean isDateSearch(LocalDateTime startDate, LocalDateTime endDate) {
-        return !(startDate.equals(LocalDateTime.MIN) && endDate.equals(LocalDateTime.MAX));
-    }
-    
-    private boolean isFloatingTask(LocalDateTime taskEndDate) {
-        return taskEndDate == null;
-    }
-    
-    private boolean isDeadlineTask(LocalDateTime taskStartDate) {
-        return taskStartDate == null;
-    }
-    
-    private boolean isNotInInterval(boolean startIsAfter, boolean startIsOn, 
-            boolean endIsBefore, boolean endIsOn) {
-        return !((startIsAfter || startIsOn) && (endIsBefore || endIsOn));
+        String feedback = storageSearch.search(tasks, searchObj, lastIdNumber);
+        return feedback;
     }
     
     public void writeToFile() {
         storageIO.writeToFile(tasks);
     }
-
 
 }
