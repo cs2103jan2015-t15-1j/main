987d7ad21743147bbc3b3203911be34c68128e1c
Sandra Liljeqvist <sandra.liljeqvist@gmail.com>
2015-04-02 13:07:30 +0800

    removed unused method from Logic

--- CS2103_gtd/src/Logic.java
+++ CS2103_gtd/src/Logic.java
@@ -7,8 +7,6 @@ public class Logic {
 	private Storage storage;
 	private History history;
 	private Translator translator;
-	private String defaultFilePath = "storage_file.json";
-	private String hiddenFilePath = "hiddenFilePath.txt";
 
 	private Logic() {
 
@@ -27,24 +25,6 @@ public class Logic {
 		history = new History();
 		translator = new Translator(storage,history);
 		return initializationFeedback;
-	}
-
-	private String getFilePath() {
-		// Todo: get current file path from a file
-		String filePath  = defaultFilePath;
-		String filename = hiddenFilePath;
-		try {
-			File datafile = new File(filename);
-			Scanner filepathScanner = new Scanner(datafile);
-			String newFilePath = filepathScanner.nextLine().trim();
-			filepathScanner.close();
-			if(!newFilePath.equals("")){
-				filePath = newFilePath;
-			}
-		} catch (Exception e) {
-			return filePath;
-		}		
-		return filePath;
 	}
 
 	public String execute(String userInput) {
